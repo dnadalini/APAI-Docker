5021c1179646a1ca7bbd1a71310c3b7ac5578c79
Davide Nadalini <d.nadalini@unibo.it>
2025-09-15 16:54:51 +0200

    Add config files for the container

--- install.sh
+++ install.sh
@@ -2,3 +2,15 @@
 wget https://github.com/pulp-platform/pulp-riscv-gnu-toolchain/releases/download/v1.0.16/v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
 tar -xvf v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2 -C ./
 mv v1.0.16-pulp-riscv-gcc-ubuntu-18 pulp-riscv-gcc-toolchain
+
+# Build PULP-SDK
+CURR_PATH=$(pwd)
+USR_PATH=$(pwd)/pulp-riscv-gcc-toolchain
+export PULP_RISCV_GCC_TOOLCHAIN=$USR_PATH
+export PATH=$USR_PATH/bin:$PATH
+source pulp-riscv-gcc-toolchain/sourceme.sh
+source pulp-sdk/configs/pulp-open.sh
+cd pulp-sdk/ 
+git submodule update --init --recursive
+make build
+cd ..
