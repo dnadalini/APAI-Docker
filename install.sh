fc035764bd6fee2b02647a10c5685efb843569cb
Davide Nadalini <d.nadalini@unibo.it>
2025-09-15 15:59:55 +0200

    Add initial files to the Docker setup

---
+++ install.sh
@@ -0,0 +1,4 @@
+# Setup PULP-RISC-V-GCC-TOOLCHAIN
+wget https://github.com/pulp-platform/pulp-riscv-gnu-toolchain/releases/download/v1.0.16/v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
+tar -xvf v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2 -C ./
+mv v1.0.16-pulp-riscv-gcc-ubuntu-18 pulp-riscv-gcc-toolchain
