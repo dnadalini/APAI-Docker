7ce5b8dd2a3351939dfcf11a3b0e3942af74f538
Davide Nadalini <d.nadalini@unibo.it>
2025-09-16 12:00:40 +0200

    Fix setup in the pulp-sdk installation file

--- install.sh
+++ install.sh
@@ -7,13 +7,15 @@ rm -rf v1.0.16-pulp-riscv-gcc-ubuntu-18
 git config --global --add safe.directory /workspaces/APAI-Docker/pulp-sdk
 
 # Build PULP-SDK
+cd pulp-sdk/ 
+git submodule update --init --recursive
+cd ..
 CURR_PATH=$(pwd)
 USR_PATH=$(pwd)/pulp-riscv-gcc-toolchain
 export PULP_RISCV_GCC_TOOLCHAIN=$USR_PATH
 export PATH=$USR_PATH/bin:$PATH
 source pulp-riscv-gcc-toolchain/sourceme.sh
 source pulp-sdk/configs/pulp-open.sh
-cd pulp-sdk/ 
-git submodule update --init --recursive
+cd pulp-sdk/
 make build
 cd ..
