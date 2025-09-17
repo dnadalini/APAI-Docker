12e654968082d5c14319805ac12002967ab83fcc
Davide Nadalini <d.nadalini@unibo.it>
2025-09-17 11:20:08 +0200

    Add backup configuration for docker

--- setup-pulp-sdk.sh
+++
@@ -1,6 +0,0 @@
-USR_PATH=$(pwd)/pulp-riscv-gcc-toolchain
-export PULP_RISCV_GCC_TOOLCHAIN=$USR_PATH
-export PATH=$USR_PATH/bin:$PATH
-source pulp-riscv-gcc-toolchain/sourceme.sh
-source pulp-sdk/configs/pulp-open.sh
-
